10e8e330aed07b3f91c60b2212464c22d09d19f1
Ketan Sethi <ketansethi786@gmail.com>
2018-11-10 18:32:42 +0530

    first commit

---
+++ pages/HomePage.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Selenium on 11-03-2016.
+ */
+require('../util/customlocators.js');
+
+var HomePage = function () {
+    this.loginAsCustomer = function () {
+        element(by.partialButtonText("Customer")).click();
+    };
+
+    this.loginAsBankManager = function () {
+        element(by.ngClick("manager()")).click();
+        return require('./AddCustomerDetails.js');
+    };
+};
+module.exports = new HomePage();
